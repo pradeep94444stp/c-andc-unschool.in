a2857088e9621a87d53330eba6772ac166eae574
pradeep94444stp <66556371+pradeep94444stp@users.noreply.github.com>
2020-08-05 17:26:35 +0530

    task 1

---
+++ unschool pradeep.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c,sum;
+    printf("sum=");
+    scanf("%d %d %d",&a,&b,&c);
+    sum=a+b+c;
+    printf("%d",sum);
+    return 0;
+}
